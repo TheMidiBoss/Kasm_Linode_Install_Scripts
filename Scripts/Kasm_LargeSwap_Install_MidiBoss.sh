5034f312412701cf017ff222ffe569d059c24b92
Joseph <Joseph@TheMidiBoss.com>
2022-02-01 00:49:32 -0500

    Basic script done, and published. Still learning git TODO what is a deployment anyway, that so did not make anything public. anyway, working on the intermediate script now. The advanced script should be a breeze by then. :)

--- Scripts/Kasm_LargeSwap_Install_MidiBoss.sh
+++ Scripts/Kasm_LargeSwap_Install_MidiBoss.sh
@@ -23,7 +23,51 @@ echo "                          #########   ########   ########   ########   "
 
 
 echo "MidiBoss - apt get update and dist-upgrade to freshen up the ubuntu"
-sudo apt-get update && sudo apt-get dist-upgrade
+# -q is no animation, -y is accept
+sudo apt-get update && sudo apt-get -q dist-upgrade -y
+
+########################################################################
+## Quoted from
+##In Docker you have 2 alternatives: (1) run each command separately
+# rather than with && joining them (might work, but it's not the "docker
+# way") (2) redirect the output to /dev/null like in the other answer.
+# Option 2 is probably your best bet while there is this bug
+##
+##########################################################################
+
+echo "Midiboss - Hardening the server ----------------------------------------"
+echo "Midiboss - Setting hostname to 'BossKasm' ------------------------------"
+hostnamectl set-hostname BossKasm
+
+
+#echo "Midiboss - Getting IP from hostname for Hostfile ----------------------"
+# hostname -I
+
+
+echo "Midiboss - Adding Default user"
+useradd KasmBoss -m
+echo -e "Change4Me!\nChange4Me!" | passwd KasmBoss
+
+echo "Midiboss - add user to sudo group"
+usermod -a -G sudo KasmBoss
+
+
+echo "Midiboss - install and setup fail2ban"
+sudo apt install fail2ban
+echo -e "
+[DEFAULT]
+destemail = <your email address>
+sendername = Fail2Ban
+sudo systemctl restart fail2ban
+
+[sshd]
+enabled = true
+port = 444
+
+[sshd-ddos]
+enabled = true
+port = 444
+" >> /etc/fail2ban/jail.local
 
 echo "MidiBoss changing 1 gig swap to 5 gig for ability to host 5 instances "
 sudo dd if=/dev/zero bs=1M count=5120 of=/mnt/5GiB.swap
