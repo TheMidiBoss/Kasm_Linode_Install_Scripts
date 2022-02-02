73050ef385a9ab130178e7d7d99e199b1127c522
Joseph <Joseph@TheMidiBoss.com>
2022-02-01 21:28:53 -0500

    Intermediate script hardening done. now to test on system and see if we can make the next steps twords ghetting inginx involved. loading to uraharas and testing.

--- README.md
+++ README.md
@@ -8,10 +8,16 @@ https://cloud.linode.com/stackscripts/964873
 ## Kasm_Basic_Install - 
     This Script runs a basic Kasm single server installation on to an Ubuntu 20.04 LTS image 
     as outlined on the Kasm Docs...
-    https://kasmweb.com/docs/latest/install/single_server_install.html
+
+https://kasmweb.com/docs/latest/install/single_server_install.html
+
     The usernames and generated passwords can be found after the installation finishes in the
-    SSout.txt file in the root directory. Login to the lish console with your root password
+    SSout.txt file in the root directory. Login to the lish console with your root password 
     and type " vi SSout.txt" then page down to the bottom of the file to find the admin@kasm.local
     and user@kasm.local passwords. (esc : q enter to quit VI) The image takes a few minutes to 
     download, do not reboot the server, you will be given a login prompt on the lish console 
     once it is ready.
+
+## Kasm_LargeSwap_Install - 
+    this Script runs the basic install scripts plus it hardens your linode server and upgrades 
+    the size of the swap and sets a default sudo user outsie off the root. 
--- Scripts/Kasm_LargeSwap_Install_MidiBoss.sh
+++ Scripts/Kasm_LargeSwap_Install_MidiBoss.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+#todo add firewall
+
 # Writing System.out (0) and System.error (2) to flat files in the root directory
 exec >/root/SSout.txt 2>/root/SSerr.txt
 
+MidiUserName="KasmBoss"
+MidiPassword="Change4Me!"
+MidiDomainName="uraharas.net"
+MidiHostName="MidiKasm"
+MidiEmailfail2ban="Joseph@TheMidiBoss.com"
+MidiIPAddress=$(hostname -I | awk '{print $1 }')
+echo "$MidiIPAddress - is ths ip address right now............................................."
 # Including this for fun
 # Every ECHO in this script will be prefixed with "MidiBoss - " to differentiate from commands
 
@@ -21,10 +30,11 @@ echo "                          +#+    +#+ +#+    +#+        +#+        +#+  "
 echo "                          #+#    #+# #+#    #+# #+#    #+# #+#    #+#  "
 echo "                          #########   ########   ########   ########   "
 
-
 echo "MidiBoss - apt get update and dist-upgrade to freshen up the ubuntu"
 # -q is no animation, -y is accept
 sudo apt-get update && sudo apt-get -q dist-upgrade -y
+#sudo install tree
+#sudo install maven
 
 ########################################################################
 ## Quoted from
@@ -35,64 +45,23 @@ sudo apt-get update && sudo apt-get -q dist-upgrade -y
 ##
 ##########################################################################
 
-echo "Midiboss - Hardening the server ----------------------------------------"
-echo "Midiboss - Setting hostname to 'MidiKasm' ------------------------------"
-hostnamectl set-hostname MidiKasm
+echo "MidiBoss - Hardening the server ----------------------------------------"
+echo "MidiBoss - locking root password, we may need to comment this out.  ----------------------------------------"
+passwd --lock root
+echo "MidiBoss - Setting hostname to 'MidiKasm' ------------------------------"
+hostnamectl set-hostname $MidiHostName
+echo "MidiBoss - Adding Default super user 'KasmBoss' ------------------------"
+adduser $MidiUserName --disabled-password --gecos ""
+echo -e "$MidiPassword\n$MidiPassword" | passwd $MidiUserName
+echo "MidiBoss - add user to sudo group------------------------------------------"
+usermod -a -G sudo $MidiUserName
 
-
-#echo "Midiboss - Getting IP from hostname for Hostfile ----------------------"
-# hostname -I
-
-                             
-echo "Midiboss - Adding Default super user 'KasmBoss' "
-sudo useradd -m KasmBoss
-echo -e "Change4Me!\nChange4Me!" | passwd KasmBoss
-
-echo "Midiboss - add user to sudo group"
-usermod -a -G sudo KasmBoss                           
-
-
-echo "Midiboss - install and setup fail2ban"
-echo "Midiboss - use 'sudo fail2ban-client status' and 'sudo fail2ban-client status sshd' to check bans"
-sudo apt install fail2ban -y
-echo -e "
-[DEFAULT]
-destemail = Joseph@TheMidiBoss.com
-sendername = Fail2Ban
-sudo systemctl restart fail2ban
-
-[sshd]
-enabled = true
-port = 444
-
-[sshd-ddos]
-enabled = true
-port = 444
-" >> /etc/fail2ban/jail.local
-
-echo "MidiBoss changing 1 gig swap to 5 gig for ability to host 5 instances "
-#sudo dd if=/dev/zero bs=1M count=5120 of=/mnt/5GiB.swapsudo chmod 600 /mnt/5GiB.swap
-#sudo mkswap /mnt/5GiB.swap
-#sudo swapon /mnt/5GiB.swap
-
-echo "MidiBoss To make the swap file available on boot"
-echo '/mnt/5GiB.swap swap swap defaults 0 0' | sudo tee -a /etc/fstab
-
-echo "MidiBoss Download the latest version of Kasm Workspaces to /tmp//Extract the package and run the installation script."
-echo "MidiBoss Default port 433 is initialised, can change"
-cd /tmp || exit
-sudo wget https://kasm-static-content.s3.amazonaws.com/kasm_release_1.10.0.238225.tar.gz
-tar -xf kasm_release*.tar.gz
-
-sudo bash kasm_release/install.sh -e
-
-echo "MidiBoss -Secure SSH from Linode setup guide"          
-sed -i -e 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
-sed -i -e 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
-systemctl restart sshd
-
-
-echo "MidiBoss - Make Scary Banner"
+echo "MidiBoss -Secure SSH from Linode setup guide----------------------------"
+sed -i -e "s/PermitRootLogin yes/PermitRootLogin no/" /etc/ssh/sshd_config
+sed -i -e "s/#PermitRootLogin no/PermitRootLogin no/" /etc/ssh/sshd_config
+sed -i -e "s/PasswordAuthentication yes/PasswordAuthentication no/" /etc/ssh/sshd_config
+sed -i -e "s/#PasswordAuthentication no/PasswordAuthentication no/" /etc/ssh/sshd_config
+echo "MidiBoss - Make Scary SSH Banner"
 echo -e "
 ::::    ::::  ::::::::::: ::::::::: :::::::::::
 +:+:+: :+:+:+     :+:     :+:    :+:    :+:
@@ -110,14 +79,51 @@ echo -e "
                           #########   ########   ########   ########
 -----------------------------------------------------------------------
 
-
 Thank you for flying MidiBoss!!
-" >>  /etc/ssh/sshd-banner
-
-#First time login info...
-#      User - KasmBoss
-#      pass - Change4Me!
-s
-ed -i -e 's/#Banner none/Banner /etc/ssh/sshd-banner/g' /etc/ssh/sshd_config
+" >>/etc/ssh/sshd-banner
+echo "MidiBoss - setting banner to sshd config"
+sed -i -e 's/#Banner none/Banner /etc/ssh/sshd-banner/g' /etc/ssh/sshd_config
 Banner /etc/ssh/sshd-banner
+echo "MidiBoss - Restart SSHD service to lock in settings"
 systemctl restart sshd
+
+echo "MidiBoss - set hosts file"
+echo -e "$MidiIPAddress\t$MidiDomainName $MidiHostName" >> /etc/hosts
+
+
+echo "MidiBoss - install and setup fail2ban"
+echo "MidiBoss - use 'sudo fail2ban-client status' and 'sudo fail2ban-client status sshd' to check bans"
+sudo apt install fail2ban -y
+echo -e "
+  [DEFAULT]
+  destemail = $MidiEmailfail2ban
+  sendername = Fail2Ban
+  sudo systemctl restart fail2ban
+
+  [sshd]
+  enabled = true
+  port = 444
+
+  [sshd-ddos]
+  enabled = true
+  port = 444
+  " >>/etc/fail2ban/jail.local
+
+echo "MidiBoss - Hardening complete, Setting up Kasm "
+echo "MidiBoss - changing 1 gig swap to 5 gig for ability to host 5 instances "
+sudo dd if=/dev/zero bs=1M count=5120 of=/mnt/5GiB.swapsudo chmod 600 /mnt/5GiB.swap
+sudo mkswap /mnt/5GiB.swap
+sudo swapon /mnt/5GiB.swap
+
+echo "MidiBoss To make the swap file available on boot"
+echo '/mnt/5GiB.swap swap swap defaults 0 0' | sudo tee -a /etc/fstab
+
+echo "MidiBoss Download the latest version of Kasm Workspaces to /tmp//Extract the package and run the installation script."
+echo "MidiBoss Default port 433 is initialised, can change"
+cd /tmp || exit
+sudo wget https://kasm-static-content.s3.amazonaws.com/kasm_release_1.10.0.238225.tar.gz
+tar -xf kasm_release*.tar.gz
+
+sudo bash kasm_release/install.sh -e
+
+sudo reboot
