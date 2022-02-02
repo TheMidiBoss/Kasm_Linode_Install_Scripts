74b0d5cbc62f8943dd2128d41e3fefbe7755043c
Joseph <Joseph@TheMidiBoss.com>
2022-02-02 18:43:53 -0500

    Intermediate script hardening done. Seens ti work just fine now. Moving to work on nginx install and then we will put kasm on top of that. might have to shift the server hardening up in the stack into its own file.
    updated the stack script with the nginx as the fast loader.

--- Scripts/Nginx_Install_Package.sh
+++ Scripts/Nginx_Install_Package.sh
@@ -28,10 +28,51 @@ echo "                          #########   ########   ########   ########   "
 echo "MidiBoss - apt get update and dist-upgrade to freshen up the ubuntu"
 # -q is no animation, -y is accept
 sudo apt-get -q update  > /root/1update.log
-sudo apt-get -q dist-upgrade -y > /root/2upgrade.log
+#sudo apt-get -qq dist-upgrade -y > /root/2upgrade.log
 
 echo "MidiBoss - set hosts file"
 echo -e "$MidiIPAddress\t$MidiDomainName $MidiHostName" >> /etc/hosts
+
+
+echo "MidiBoss - New Stuff here"
+echo sudo apt-get install -y docker
+echo sudo apt-get -q install -y docker-compose
+
+echo mkdir /opt/nginxproxymanager
+cd /opt/nginxproxymanager
+echo -e '
+version: '3'
+services:
+  app:
+    image: 'jc21/nginx-proxy-manager:latest'
+    ports:
+      - '80:80'
+      - '81:81'
+      - '443:443'
+    environment:
+      DB_MYSQL_HOST: "db"
+      DB_MYSQL_PORT: 3306
+      DB_MYSQL_USER: "npm"
+      DB_MYSQL_PASSWORD: "npm"
+      DB_MYSQL_NAME: "npm"
+    volumes:
+      - ./data:/data
+      - ./letsencrypt:/etc/letsencrypt
+  db:
+    image: 'jc21/mariadb-aria:latest'
+    environment:
+      MYSQL_ROOT_PASSWORD: 'npm'
+      MYSQL_DATABASE: 'npm'
+      MYSQL_USER: 'npm'
+      MYSQL_PASSWORD: 'npm'
+    volumes:
+      - ./data/mysql:/var/lib/mysql
+
+' >> docker-compose.yml
+
+docker-compose up -d
+
+
 
 touch /root/all.done
 sudo reboot
