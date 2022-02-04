3aeb5908c3e2dd7111b4fad57d4d0969164db7bf
Joseph <Joseph@TheMidiBoss.com>
2022-02-04 17:07:47 -0500

    Ngnix working and kasm! nginx over lareKasm_LargeSwap_Install_MidiBoss.shge script dont, testing

--- MidiScripts/Linode_Quick_StackScript_GitHub_Raw.sh
+++ MidiScripts/Linode_Quick_StackScript_GitHub_Raw.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-bash <( curl -sk https://raw.githubusercontent.com/TheMidiBoss/Kasm_Linode_Install_Scripts/main/Scripts/Kasm_LargeSwap_Install_MidiBoss.sh )
+bash <( curl -sk https://raw.githubusercontent.com/TheMidiBoss/Kasm_Linode_Install_Scripts/main/MidiScripts/Kasm_LargeSwap_Install_MidiBoss.sh )
