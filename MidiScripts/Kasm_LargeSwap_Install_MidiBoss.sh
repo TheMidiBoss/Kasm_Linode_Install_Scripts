c530006888ce097eac21d1596f0f9695c76a8047
Joseph <Joseph@TheMidiBoss.com>
2022-02-04 23:03:03 -0500

    Ngnix working and kasm! nginx over lareKasm_LargeSwap_Install_MidiBoss.shge script dont, testing

--- MidiScripts/Kasm_LargeSwap_Install_MidiBoss.sh
+++ MidiScripts/Kasm_LargeSwap_Install_MidiBoss.sh
@@ -37,90 +37,90 @@ sudo apt-get -q dist-upgrade -y > /root/2.AptGetUpgrade.log
 
 
 echo "MidiBoss - Hardening the server ----------------------------------------"
-    echo "MidiBoss - locking root password, we may need to comment this out.  ----------------------------------------"
-      #passwd --lock root
+      echo "MidiBoss - locking root password, we may need to comment this out.  ----------------------------------------"
+          #passwd --lock root
 
-    echo "MidiBoss - Setting hostname to 'MidiKasm' ------------------------------"
-      sudo hostnamectl set-hostname $MidiHostName
+      echo "MidiBoss - Setting hostname to 'MidiKasm' ------------------------------"
+          sudo hostnamectl set-hostname $MidiHostName
 
-    echo "MidiBoss - Adding Default super user 'KasmBoss' ------------------------"
-      sudo useradd -m $MidiUserName
-      echo -e "$MidiPassword\n$MidiPassword" | passwd $MidiUserName
+      echo "MidiBoss - Adding Default super user 'KasmBoss' ------------------------"
+          sudo useradd -m $MidiUserName
+          echo -e "$MidiPassword\n$MidiPassword" | passwd $MidiUserName
 
-    echo "MidiBoss - add user to sudo group------------------------------------------"
-      usermod -a -G sudo $MidiUserName
+      echo "MidiBoss - add user to sudo group------------------------------------------"
+          usermod -a -G sudo $MidiUserName
 
-    echo "MidiBoss -Secure SSH from Linode setup guide----------------------------"
-      sed -i -e "s/PermitRootLogin yes/PermitRootLogin no/" /etc/ssh/sshd_config
-      sed -i -e "s/#PermitRootLogin no/PermitRootLogin no/" /etc/ssh/sshd_config
-      sed -i -e "s/PasswordAuthentication yes/PasswordAuthentication no/" /etc/ssh/sshd_config
-      sed -i -e "s/#PasswordAuthentication no/PasswordAuthentication no/" /etc/ssh/sshd_config
+      echo "MidiBoss -Secure SSH from Linode setup guide----------------------------"
+          sed -i -e "s/PermitRootLogin yes/PermitRootLogin no/" /etc/ssh/sshd_config
+          sed -i -e "s/#PermitRootLogin no/PermitRootLogin no/" /etc/ssh/sshd_config
+          sed -i -e "s/PasswordAuthentication yes/PasswordAuthentication no/" /etc/ssh/sshd_config
+          sed -i -e "s/#PasswordAuthentication no/PasswordAuthentication no/" /etc/ssh/sshd_config
 
-    echo "MidiBoss - Make Scary SSH Banner"
-      #/etc/ssh/sshd-banner
-      #echo "MidiBoss - setting banner to sshd config" #todo add banner stuff back in
-      #sed -i -e "s%'#Banner none'%'Banner /etc/ssh/sshd-banner'%g" /etc/ssh/sshd_config
+      echo "MidiBoss - Make Scary SSH Banner"
+          #/etc/ssh/sshd-banner
+          #echo "MidiBoss - setting banner to sshd config" #todo add banner stuff back in
+          #sed -i -e "s%'#Banner none'%'Banner /etc/ssh/sshd-banner'%g" /etc/ssh/sshd_config
 
-    echo "MidiBoss - Restart SSHD service to lock in settings"
-      systemctl restart sshd
+      echo "MidiBoss - Restart SSHD service to lock in settings"
+          systemctl restart sshd
 
-    echo "MidiBoss - set hosts file"
-    echo -e "$MidiIPAddress\t$MidiDomainName $MidiHostName" >> /etc/hosts
+      echo "MidiBoss - set hosts file"
+          echo -e "$MidiIPAddress\t$MidiDomainName $MidiHostName" >> /etc/hosts
 
-    echo "MidiBoss - install and setup fail2ban"
-    echo "MidiBoss - use 'sudo fail2ban-client status' and 'sudo fail2ban-client status sshd' to check bans"
-    sudo apt-get install fail2ban -y > /root/3fail2ban.log
-    echo -e "
-      [DEFAULT]
-      destemail = $MidiEmailfail2ban
-      sendername = Fail2Ban
-      sudo systemctl restart fail2ban
+      echo "MidiBoss - install and setup fail2ban"
+          echo "MidiBoss - use 'sudo fail2ban-client status' and 'sudo fail2ban-client status sshd' to check bans"
+          sudo apt-get install fail2ban -y > /root/3fail2ban.log
+          echo -e "
+[DEFAULT]
+destemail = $MidiEmailfail2ban
+sendername = Fail2Ban
+sudo systemctl restart fail2ban
 
-      [sshd]
-      enabled = true
-      port = 444
+[sshd]
+enabled = true
+port = 444
 
-      [sshd-ddos]
-      enabled = true
-      port = 444
-      " >>/etc/fail2ban/jail.local
+[sshd-ddos]
+enabled = true
+port = 444
+" >>/etc/fail2ban/jail.local
 
 echo "MidiBoss - Hardening complete, Setting up Nginx "
     echo "MidiBoss - Install Docker Stuff here"
-      sudo apt-get install -y docker
-      sudo apt-get -q install -y docker-compose
+        sudo apt-get install -y docker
+        sudo apt-get -q install -y docker-compose
 
       echo "MidiBoss - Make the working directory for docker"
-        mkdir /opt/nginxproxymanager
-        cd /opt/nginxproxymanager || exit
+          mkdir /opt/nginxproxymanager
+          cd /opt/nginxproxymanager || exit
 
       echo "MidiBoss - Download the docker compose file from Github"
-        sudo wget $MidiDockerComposeYamlURL
+          sudo wget $MidiDockerComposeYamlURL
 
       echo "MidiBoss - Docker it UP"
-        docker-compose up -d
+          docker-compose up -d
 
 echo "MidiBoss - Nginx complete, Setting up Kasm "
-  echo "MidiBoss - changing 1 gig swap to 5 gig for ability to host 5 instances "
-    sudo dd if=/dev/zero bs=1M count=5120 of=/mnt/5GiB.swap
-    sudo chmod 600 /mnt/5GiB.swap
-    sudo mkswap /mnt/5GiB.swap
-    sudo swapon /mnt/5GiB.swap
+    echo "MidiBoss - changing 1 gig swap to 5 gig for ability to host 5 instances "
+        sudo dd if=/dev/zero bs=1M count=5120 of=/mnt/5GiB.swap
+        sudo chmod 600 /mnt/5GiB.swap
+        sudo mkswap /mnt/5GiB.swap
+        sudo swapon /mnt/5GiB.swap
 
-  echo "MidiBoss - To make the swap file available on boot"
-    echo -e "/mnt/5GiB.swap\tswap\t\tswap\tdefaults\t  0\t0" | sudo tee -a /etc/fstab
+    echo "MidiBoss - To make the swap file available on boot"
+        echo -e "/mnt/5GiB.swap\tswap\t\tswap\tdefaults\t  0\t0" | sudo tee -a /etc/fstab
 
-  echo "MidiBoss - Download the latest version of Kasm Workspaces to /tmp"
-    cd /tmp || exit
-    sudo wget $MidiKasmLatestVersionUrl
+    echo "MidiBoss - Download the latest version of Kasm Workspaces to /tmp"
+        cd /tmp || exit
+        sudo wget $MidiKasmLatestVersionUrl
 
-  echo "MidiBoss - Extract the package and duplicate it to the save folder (can remove later after dev)"
-    tar -xf kasm*.tar.gz
-    mkdir save
-    cp -r kasm_release save
+    echo "MidiBoss - Extract the package and duplicate it to the save folder (can remove later after dev)"
+        tar -xf kasm*.tar.gz
+        mkdir save
+        cp -r kasm_release save
 
-  echo "MidiBoss - Run the installation script. Changed to port 8433 is initialised, TODO change to $"
-    sudo bash kasm_release/install.sh -e -L 8433 -I -P $MidiPassword -p $MidiDomainName  #TODO change 8443 to $"
+    echo "MidiBoss - Run the installation script. Changed to port 8433 is initialised, TODO change to $"
+        sudo bash kasm_release/install.sh -e -L 8433 -I -P $MidiPassword -p $MidiDomainName  #TODO change 8443 to $"
 
 cd /tmp || exit
 echo "MidiBoss - Install my longview link ----------- Only works on fresh longview sessions?"
