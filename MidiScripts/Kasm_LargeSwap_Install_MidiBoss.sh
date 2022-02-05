bf61d88af0d969876be8ea9d32bab78b7043872d
Joseph <Joseph@TheMidiBoss.com>
2022-02-05 00:36:13 -0500

    Ngnix working and kasm! nginx over lareKasm_LargeSwap_Install_MidiBoss.shge script dont, testing

--- MidiScripts/Kasm_LargeSwap_Install_MidiBoss.sh
+++ MidiScripts/Kasm_LargeSwap_Install_MidiBoss.sh
@@ -128,7 +128,14 @@ echo "MidiBoss - Nginx complete, Setting up Kasm "
 
     echo "MidiBoss - Kasm Done installing portainer"
       cd ~/ || exit
-      docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+      docker volume create portainer_data
+      #docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+docker run -d -p 8000:8000 -p 9444:9444 --name portainer \
+    --restart=always \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v portainer_data:/data \
+    cr.portainer.io/portainer/portainer-ce:2.11.0
+
 
 cd /tmp || exit
 echo "MidiBoss - Install my longview link ----------- Only works on fresh longview sessions?"
