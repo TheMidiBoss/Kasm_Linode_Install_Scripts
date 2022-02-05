b523d13da1499eacbaf6a76f70633d5bb366193a
Joseph <Joseph@TheMidiBoss.com>
2022-02-05 00:37:42 -0500

    Ngnix working and kasm! nginx over lareKasm_LargeSwap_Install_MidiBoss.shge script dont, testing

--- MidiScripts/Kasm_LargeSwap_Install_MidiBoss.sh
+++ MidiScripts/Kasm_LargeSwap_Install_MidiBoss.sh
@@ -130,7 +130,7 @@ echo "MidiBoss - Nginx complete, Setting up Kasm "
       cd ~/ || exit
       docker volume create portainer_data
       #docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
-docker run -d -p 8000:8000 -p 9444:9444 --name portainer \
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer \
     --restart=always \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v portainer_data:/data \
